bb4662307d7674883ef2dfe430ac138807ffbd35
Jason Hilimire <jasonhilimire@mac.com>
2020-01-26 21:48:36 -0700

    DataModel for Entities Created

---
+++ DialedIn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DialedIn
+//
+//  Created by Jason Hilimire on 1/26/20.
+//  Copyright © 2020 Jason Hilimire. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
