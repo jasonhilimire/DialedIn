bf135186d61d7a754cc852d6cac10d274a5af9e4
jasonhilimire <jasonhilimire@mac.com>
2020-03-20 21:05:40 -0600

    ScrollView Setup

--- DialedIn/Views/MainViews/BikeListView/BikeListView.swift
+++ DialedIn/Views/MainViews/BikeListView/BikeListView.swift
@@ -21,36 +21,22 @@ struct BikeListView: View {
     @State private var showingAddScreen = false
 
 	var body: some View {
-		NavigationView {
-			List {
-				ForEach(bikes, id: \.self) { bike in
-					BikeStyledCellView(bike: bike)
-					.padding(.bottom, 10)
-				}
-				.onDelete(perform: deleteBike)
-			}
-			.navigationBarTitle("Bikes")
-			.navigationBarItems(leading: EditButton().foregroundColor(Color.white), trailing:
-				Button(action: {self.showingAddScreen.toggle()
-				}) {
-					Image(systemName: "plus.circle").foregroundColor(Color.white)
-			})
-				.sheet(isPresented: $showingAddScreen)  {
-					AddBikeView().environment(\.managedObjectContext, self.moc)
+		ScrollView {
+			ForEach(bikes, id: \.self) { bike in
+				BikeStyledCellView(bike: bike)
+				.padding(10)
 			}
 		}
+		.navigationBarTitle("Bikes")
+		.navigationBarItems(leading: EditButton().foregroundColor(Color.white), trailing:
+			Button(action: {self.showingAddScreen.toggle()
+			}) {
+				Image(systemName: "plus.circle").foregroundColor(Color.white)
+		})
+			.sheet(isPresented: $showingAddScreen)  {
+				AddBikeView().environment(\.managedObjectContext, self.moc)
+		}
 	}
-    
-    func deleteBike(at offsets: IndexSet) {
-        for offset in offsets {
-            // find this note in our fetch request
-            let bike = bikes[offset]
-            // delete it from the context
-            moc.delete(bike)
-        }
-        // save the context
-        try? moc.save()
-    }
 }
 
 struct BikeListView_Previews: PreviewProvider {
@@ -60,58 +46,4 @@ struct BikeListView_Previews: PreviewProvider {
 }
 
 
-/*  this is for scroll view
-import SwiftUI
 
-struct BikeListView: View {
-	
-	// Create the MOC
-	@Environment(\.managedObjectContext) var moc
-	// create a Fetch request for Bike
-	@FetchRequest(entity: Bike.entity(), sortDescriptors: [
-		NSSortDescriptor(keyPath: \Bike.name, ascending: true)
-	]) var bikes: FetchedResults<Bike>
-	
-	// bool to show the Sheet
-	@State private var showingAddScreen = false
-	
-	var body: some View {
-		NavigationView {
-			List {
-				ForEach(bikes, id: \.self) { bike in
-					NavigationLink(destination: BikeDetailView()) {
-						VStack(alignment: .leading) {
-							Text(bike.name ?? "Unknown Bike")
-								.font(.headline)
-							Text(bike.bikeNote ?? "Some Text goes here")
-								.font(.callout)
-								.foregroundColor(.secondary)
-						}
-					}
-				}
-				.onDelete(perform: deleteBike)
-			}
-			.navigationBarTitle("Bikes")
-			.navigationBarItems(leading: EditButton(), trailing:
-				Button(action: {self.showingAddScreen.toggle()
-				}) {
-					Image(systemName: "plus.circle")
-			})
-				.sheet(isPresented: $showingAddScreen)  {
-					BikeView().environment(\.managedObjectContext, self.moc)
-			}
-		}
-	}
-	
-	func deleteBike(at offsets: IndexSet) {
-		for offset in offsets {
-			// find this note in our fetch request
-			let bike = bikes[offset]
-			// delete it from the context
-			moc.delete(bike)
-		}
-		// save the context
-		try? moc.save()
-	}
-}
-*/
