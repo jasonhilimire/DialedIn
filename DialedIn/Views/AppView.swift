2657e95f9d83e18420957eb20dbcbeae3bc9a75f
jasonhilimire <jasonhilimire@mac.com>
2020-10-05 21:04:56 -0600

    3 tabs, added mountain bike image

--- DialedIn/Views/AppView.swift
+++ DialedIn/Views/AppView.swift
@@ -21,11 +21,6 @@ struct AppView: View {
         
     var body: some View {
         TabView {
-            //TODO: Add Scrolling/Main View for recent notes? and or TopRated Notes
-            // https://www.hackingwithswift.com/quick-start/swiftui/how-to-add-horizontal-and-vertical-scrolling-using-scrollview
-            // https://www.youtube.com/watch?v=EBbhIbI2Hg8
-            //
-            
             HomeTabView()
 				.tabItem {
 					Image(systemName: "house")
@@ -38,17 +33,17 @@ struct AppView: View {
                     Text("Notes")
                 }
             
-//            BikeListView()
 				BikesView()
                 .tabItem {
-                    Image(systemName: "gear")
+//					Image("mountainbike")
+					Image(systemName: "hare")
                     Text("Bikes")
                 }
-			ServiceView()
-				.tabItem {
-					Image(systemName: "wrench")
-					Text("Service")
-			}
+//			ServiceView()
+//				.tabItem {
+//					Image(systemName: "wrench")
+//					Text("Service")
+//			}
 			
         }
 			.animation(.default)
