ebb122b06ba257679d4c8893602bad3135ab4dff
jasonhilimire <jasonhilimire@mac.com>
2021-04-14 20:51:16 -0600

    addnote sheet

--- DialedIn/Views/MainViews/AddItemsView.swift
+++ DialedIn/Views/MainViews/AddItemsView.swift
@@ -9,9 +9,19 @@
 import SwiftUI
 
 struct AddItemsView: View {
+	// Create the MOC
+	@Environment(\.managedObjectContext) var moc
+	@State private var addNoteSheet = false
+	
     var body: some View {
 		VStack{
+			Button(action: {addNoteSheet.toggle()}) {
+				Text("Add Note")
+			}
 			Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		}
+		.sheet(isPresented: $addNoteSheet)  {
+			AddNoteView().environment(\.managedObjectContext, self.moc)
 		}
     }
 }
